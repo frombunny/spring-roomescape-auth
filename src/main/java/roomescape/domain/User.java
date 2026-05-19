66d38beac77e244f2330a205e5aef80e134337ff
frombunny <rucy011121@gmail.com>
2026-05-19 19:53:14 +0900

    feat(user): User 생성 검증 로직 구현

--- src/main/java/roomescape/domain/User.java
+++ src/main/java/roomescape/domain/User.java
@@ -1,6 +1,7 @@
 package roomescape.domain;
 
 import lombok.Getter;
+import roomescape.global.exception.ValidationException;
 
 @Getter
 public class User {
@@ -19,6 +20,38 @@ public class User {
     }
 
     public static User create(String name, String loginId, String password, Role role) {
+        validate(name, loginId, password, role);
         return new User(null, loginId, password, name, role);
+    }
+
+    private static void validate(String name, String loginId, String password, Role role) {
+        validateName(name);
+        validateLoginId(loginId);
+        validatePassword(password);
+        validateRole(role);
+    }
+
+    private static void validateRole(Role role) {
+        if (role == null) {
+            throw new ValidationException("권한은 필수 값입니다.");
+        }
+    }
+
+    private static void validatePassword(String password) {
+        if (password == null || password.isBlank()) {
+            throw new ValidationException("비밀번호는 필수 값입니다.");
+        }
+    }
+
+    private static void validateLoginId(String loginId) {
+        if (loginId == null || loginId.isBlank()) {
+            throw new ValidationException("로그인 아이디는 필수 값입니다.");
+        }
+    }
+
+    private static void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new ValidationException("이름은 필수 값입니다.");
+        }
     }
 }
